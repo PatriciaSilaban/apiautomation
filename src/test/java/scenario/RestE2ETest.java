059d8fc2bc01166c45011ec5d8bb9b22a2dfe713
PatriciaSilaban <silabanlahya@gmail.com>
2025-02-21 22:56:03 +0700

    task (end to end test)

--- src/test/java/scenario/RestE2ETest.java
+++ src/test/java/scenario/RestE2ETest.java
@@ -1,105 +1,118 @@
 package scenario;
 
+import static io.restassured.RestAssured.*;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.apiautomation.model.ResponseItem;
+import com.apiautomation.model.addObjectResponse;
+import com.apiautomation.model.deleteObjectResponse;
+import com.apiautomation.model.getListSingleObjectResponse;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-import static io.restassured.RestAssured.given;
-
 public class RestE2ETest {
-        ResponseItem responseItem;
-
         /*
-         * Scenario e2e test
-         * 1. Hit add products (verify response)
-         * 2. Hit get products (verify response)
-         * 3. Hit update product (verify response)
+         * 1. Create new object (hit API add_object)
+         * 2. Verify new object is added (hit API single_object)
+         * 3. Delete product (hit API delete_object)
+         * 4. Verify new object is deleted (hit API single_object)
          */
 
         @Test
         public void scenarioE2ETest() {
                 String json = "{\n" + //
-                                "  \"id\": 1,\n" + //
-                                "  \"title\": \"Le Minerale\",\n" + //
-                                "  \"description\": \"Minuman segar dan menyehatkan.\",\n"
-                                + //
-                                "  \"category\": \"food\",\n" + //
-                                "  \"price\": 10000,\n" + //
-                                "  \"discountPercentage\": 5,\n" + //
-                                "  \"rating\": 5,\n" + //
-                                "  \"stock\": 15,\n" + //
-                                "  \"tags\": [\n" + //
-                                "    \"beauty\",\n" + //
-                                "    \"mascara\"\n" + //
-                                "  ],\n" + //
-                                "  \"dimensions\": {\n" + //
-                                "    \"width\": 23.17,\n" + //
-                                "    \"height\": 14.43,\n" + //
-                                "    \"depth\": 28.01\n" + //
-                                "  }\n" + //
+                                "   \"name\": \"Apple MacBook Pro 16\",\n" + //
+                                "   \"data\": {\n" + //
+                                "      \"year\": 2019,\n" + //
+                                "      \"price\": 20000,\n" + //
+                                "      \"CPU model\": \"Intel Core i9\",\n" + //
+                                "      \"Hard disk size\": \"1 TB\"\n" + //
+                                "   }\n" + //
                                 "}";
+                // Base URI
+                RestAssured.baseURI = "https://api.restful-api.dev";
 
-                // Add Product
-                RestAssured.baseURI = "https://dummyjson.com";
-
+                // Step 1 : Create object (addObject)
                 Response response = given()
                                 .log()
                                 .all()
-                                .pathParam("path", "products")
-                                .pathParam("method", "add")
+                                .pathParam("path", "objects")
                                 .body(json)
                                 .contentType("application/json")
                                 .when()
-                                .post("{path}/{method}");
+                                .post("{path}");
 
-                System.out.println("add product" + response.asPrettyString());
+                System.out.println("add object" + response.asPrettyString());
 
-                JsonPath addJsonPath = response.jsonPath();
-                responseItem = addJsonPath.getObject("", ResponseItem.class);
-                Assert.assertEquals(response.statusCode(), 201);
-                Assert.assertEquals(responseItem.title, "Le Minerale");
-                Assert.assertEquals(responseItem.price, 10000);
-                Assert.assertEquals(responseItem.discountPercentage, 5);
-                Assert.assertEquals(responseItem.stock, 15);
-                Assert.assertEquals(responseItem.category, "food");
+                JsonPath jsonPath = response.jsonPath();
+                addObjectResponse objectResponse = jsonPath.getObject("", addObjectResponse.class);
 
-                String idObject = responseItem.id;
-                // untuk tugas, nnti idObjectnya diganti dengan id yang didapat saat merun add
-                // product
+                Assert.assertEquals(objectResponse.name, "Apple MacBook Pro 16");
+                Assert.assertNotNull(objectResponse.createdAt);
+                Assert.assertNotNull(objectResponse.id);
+                Assert.assertEquals(objectResponse.dataItem.year, 2019);
+                Assert.assertEquals(objectResponse.dataItem.price, 20000);
+                Assert.assertEquals(objectResponse.dataItem.cpuModel, "Intel Core i9");
+                Assert.assertEquals(objectResponse.dataItem.hardDiskSize, "1 TB");
 
-                // Get Product
+                String idObject = objectResponse.id;
 
+                // Step 2 : Verify Object Exists (singleObject)
                 Response response2 = given()
-                                .pathParam("path", "products")
-                                .pathParam("idProduct", idObject)
                                 .log()
                                 .all()
+                                .pathParam("path", "objects")
+                                .pathParam("idObject", idObject)
                                 .when()
-                                .get("{path}/{idProduct}");
+                                .get("{path}/{idObject}");
 
-                System.out.println("response2" + response2.asPrettyString());
-                // Validation POJO
+                System.out.println("verify object exists" + response2.asPrettyString());
 
-                // Update Product
-                RestAssured.baseURI = "https://dummyjson.com";
+                JsonPath jsonPath2 = response2.jsonPath();
+                getListSingleObjectResponse listSingleObjectResponse2 = jsonPath2.getObject("",
+                                getListSingleObjectResponse.class);
 
+                Assert.assertEquals(listSingleObjectResponse2.id, idObject);
+                Assert.assertEquals(listSingleObjectResponse2.name, "Apple MacBook Pro 16");
+                Assert.assertEquals(listSingleObjectResponse2.dataItem.year, 2019);
+                Assert.assertEquals(listSingleObjectResponse2.dataItem.price, 20000);
+                Assert.assertEquals(listSingleObjectResponse2.dataItem.cpuModel, "Intel Core i9");
+                Assert.assertEquals(listSingleObjectResponse2.dataItem.hardDiskSize, "1 TB");
+
+                // Step 3 : Delete Object
                 Response response3 = given()
                                 .log()
                                 .all()
-                                .pathParam("path", "products")
-                                .pathParam("idProduct", idObject)
-                                .body(json)
-                                .contentType("application/json")
+                                .pathParam("path", "objects")
+                                .pathParam("idObject", idObject)
                                 .when()
-                                .put("{path}/{idProduct}");
+                                .delete("{path}/{idObject}");
 
-                System.out.println("update product" + response.asPrettyString());
-                // Validation POJO
+                System.out.println("delete object" + response3.asPrettyString());
+
+                JsonPath jsonPath3 = response3.jsonPath();
+                deleteObjectResponse deleteObject3 = jsonPath3.getObject("",
+                                deleteObjectResponse.class);
+                Assert.assertEquals(deleteObject3.message,
+                                "Object with id = " + idObject + " has been deleted.");
+
+                // Step 4 : Verify Object is deleted
+                Response response4 = given()
+                                .log()
+                                .all()
+                                .pathParam("path", "objects")
+                                .pathParam("idObject", idObject)
+                                .when()
+                                .get("{path}/{idObject}");
+
+                System.out.println("verify object is deleted" + response4.asPrettyString());
+
+                Assert.assertEquals(response4.statusCode(), 404);
+
         }
+
 }
